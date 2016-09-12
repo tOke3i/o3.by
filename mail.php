6cb9d5ef8aa824c2b1909e7e32b5feb8f194a174
tOke3i <paul.pauliuts@gmail.com>
2016-09-12 16:21:16 +0300

    php send mail

--- mail.php
+++ mail.php
@@ -1,12 +1,13 @@
 <?php
 
-$recepient = "sales@odorgon.by";
+$recepient = "sales@odorgon.by paul.paulits@gmail.com";
 $sitename = "o3.by";
 
 $name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
 $phone = trim($_POST["phone"]);
 $text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nТелефон: $phone \nEmail: $email \nТекст: $text";
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$pagetitle = "Сообщение \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
